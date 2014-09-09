1a8f0aa33f75f8655761eb7f3d18af11c422a776
Mark Tobin <mtspark@gmail.com>
2014-09-10 00:06:44 +0100

    Prototype. Working with small samples.

--- cbe-upload.py
+++ cbe-upload.py
@@ -1,11 +1,35 @@
 import _mssql
-import uuid
+import csv
+
+# Takes data directly from last stocktake (using the takestock03 db) 
+# to create the required .csv for upload to CBE
+# 
+# Format is  barcode,qty,YYYY-MM-DD,HH:mm:ss
+# Latest entry only incuded whose quantity is a sum of all entries 
+
+
 conn = _mssql.connect(server='localhost', user='sa', password='waveoman', \
     database='takestock03')
-conn.execute_query("SELECT TOP 1 gId FROM tblStocktake ORDER BY dStarted DESC;")
+conn.execute_query("SELECT TOP 1 gId, sCustomerCode FROM tblStocktake ORDER BY dStarted DESC;")
 for row in conn: 
 	g_id = str(row['gId'])
-sql = "SELECT  sBarcode, fQty, dScanned FROM tblStocktakeLineItem WHERE gStocktakeId = %s;"
+	customer = row['sCustomerCode']
+sql = "SELECT sBarcode, fQty, dScanned FROM tblStocktakeLineItem WHERE gStocktakeId = %s ORDER BY dScanned;"
 conn.execute_query(sql, g_id)
+dict = {}
 for row in conn:
-	print(str(row['sBarcode'])+', '+str(row['fQty'])+', '+str(row['dScanned']))
+	barcode = str(row['sBarcode'])
+	qty     = row['fQty']
+	date    = str(row['dScanned']).split()[0]
+	time    = str(row['dScanned']).split()[1]
+	if barcode in dict:
+		dict[barcode][0] += qty
+		dict[barcode][1] = date
+		dict[barcode][2] = time
+	else: 
+		dict[barcode] = [qty, date, time]
+conn.close(); 
+output_file = customer + ".csv"
+with open(output_file,'w') as f:
+    for key in dict: 
+    	f.write(key+","+str(dict[key][0])+","+dict[key][1]+","+dict[key][2]+"\n")
