c0bbe644d1757ba149378fae118464eafe174a83
Mark Tobin <mtspark@gmail.com>
2014-09-14 00:49:06 +0100

    Cleanup and specify path for inside VM

--- cbe-upload.py
+++ cbe-upload.py
@@ -7,7 +7,6 @@ import csv
 # Format is  barcode,qty,YYYY-MM-DD,HH:mm:ss
 # Latest entry only incuded whose quantity is a sum of all entries 
 
-
 conn = _mssql.connect(server='localhost', user='sa', password='waveoman', \
     database='takestock03')
 conn.execute_query("SELECT TOP 1 gId, sCustomerCode FROM tblStocktake ORDER BY dStarted DESC;")
@@ -16,6 +15,7 @@ for row in conn:
 	customer = row['sCustomerCode']
 sql = "SELECT sBarcode, fQty, dScanned FROM tblStocktakeLineItem WHERE gStocktakeId = %s ORDER BY dScanned;"
 conn.execute_query(sql, g_id)
+
 dict = {}
 for row in conn:
 	barcode = str(row['sBarcode'])
@@ -29,7 +29,9 @@ for row in conn:
 	else: 
 		dict[barcode] = [qty, date, time]
 conn.close(); 
-output_file = customer + ".csv"
+
+# Path set to run in virtual machine machine os 
+output_file = "C:\\Documents and Settings\\scando\\Desktop\\CBE Exports\\" + customer + ".csv"
 with open(output_file,'w') as f:
     for key in dict: 
-    	f.write(key+","+str(dict[key][0])+","+dict[key][1]+","+dict[key][2]+"\n")
+    	f.write(key+","+str(dict[key][0])+","+dict[key][1]+","+dict[key][2]+"\n"
